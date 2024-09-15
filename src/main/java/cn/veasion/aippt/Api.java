d99eb4a7075521af47bf6727eaf3cd26ab281963
veasion <veasion@qq.com>
2024-09-15 22:51:45 +0800

    modify

--- src/main/java/cn/veasion/aippt/Api.java
+++ src/main/java/cn/veasion/aippt/Api.java
@@ -2,7 +2,11 @@ package cn.veasion.aippt;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 /**
@@ -34,12 +38,41 @@ public class Api {
         return result.getJSONObject("data").getString("token");
     }
 
-    public static String generateOutline(String apiToken, String subject, String prompt, String dataUrl) {
+    public static String parseFileData(String apiToken, File file, String content, String fileUrl) {
+        String url = BASE_URL + "/api/ppt/parseFileData";
+        HttpUtils.HttpRequest httpRequest = new HttpUtils.HttpRequest();
+        httpRequest.setUrl(url);
+        httpRequest.setMethod("POST");
+        httpRequest.addHeaders("token", apiToken);
+        MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create();
+        multipartEntity.setCharset(StandardCharsets.UTF_8);
+        if (file != null) {
+            multipartEntity.addBinaryBody("file", file);
+        }
+        if (content != null) {
+            multipartEntity.addTextBody("content", content, ContentType.create("text/plain", StandardCharsets.UTF_8));
+        }
+        if (fileUrl != null) {
+            multipartEntity.addTextBody("fileUrl", fileUrl, ContentType.create("text/plain", StandardCharsets.UTF_8));
+        }
+        httpRequest.setBody(multipartEntity.build());
+        HttpUtils.HttpResponse response = HttpUtils.request(httpRequest);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("解析文件或内容失败，httpStatus=" + response.getStatus());
+        }
+        JSONObject result = response.getResponseToJson();
+        if (result.getIntValue("code") != 0) {
+            throw new RuntimeException("解析文件或内容异常，" + result.getString("message"));
+        }
+        return result.getJSONObject("data").getString("dataUrl");
+    }
+
+    public static String generateOutline(String apiToken, String subject, String dataUrl, String prompt) {
         String url = BASE_URL + "/api/ppt/generateOutline";
         JSONObject body = new JSONObject();
         body.put("subject", subject);
-        body.put("prompt", prompt);
         body.put("dataUrl", dataUrl);
+        body.put("prompt", prompt);
         HttpUtils.HttpRequest httpRequest = HttpUtils.HttpRequest.postJson(url);
         httpRequest.setBody(body.toJSONString());
         httpRequest.addHeaders("token", apiToken);
@@ -67,12 +100,12 @@ public class Api {
         return sb.toString();
     }
 
-    public static String generateContent(String apiToken, String outlineMarkdown, String prompt, String dataUrl) {
+    public static String generateContent(String apiToken, String outlineMarkdown, String dataUrl, String prompt) {
         String url = BASE_URL + "/api/ppt/generateContent";
         JSONObject body = new JSONObject();
         body.put("outlineMarkdown", outlineMarkdown);
-        body.put("prompt", prompt);
         body.put("dataUrl", dataUrl);
+        body.put("prompt", prompt);
         HttpUtils.HttpRequest httpRequest = HttpUtils.HttpRequest.postJson(url);
         httpRequest.setBody(body.toJSONString());
         httpRequest.addHeaders("token", apiToken);
@@ -161,14 +194,14 @@ public class Api {
         return result.getJSONObject("data").getString("fileUrl");
     }
 
-    public static JSONObject directGeneratePptx(String apiToken, boolean stream, String templateId, String subject, String prompt, String dataUrl, boolean pptxProperty) {
+    public static JSONObject directGeneratePptx(String apiToken, boolean stream, String templateId, String subject, String dataUrl, String prompt, boolean pptxProperty) {
         String url = BASE_URL + "/api/ppt/directGeneratePptx";
         JSONObject body = new JSONObject();
         body.put("stream", stream);
         body.put("templateId", templateId);
         body.put("subject", subject);
-        body.put("prompt", prompt);
         body.put("dataUrl", dataUrl);
+        body.put("prompt", prompt);
         body.put("pptxProperty", pptxProperty);
         HttpUtils.HttpRequest httpRequest = HttpUtils.HttpRequest.postJson(url);
         httpRequest.setBody(body.toJSONString());
